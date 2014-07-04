ce7326b72ce82f123753fdb619eaece549ef5079
bkpabi <bkpabi@gmail.com>
2014-07-04 16:33:37 +0530

    Added class UserDetailDTO

---
+++ Compass.Business.Entities/UserDetailDTO.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compass.Business.Entities
+{
+    using System.Runtime.Serialization;
+    [DataContract]
+    public class UserDetailDTO
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public string Email { get; set; }
+
+        [DataMember]
+        public string Password { get; set; }
+
+        [DataMember]
+        public string MobileNumber { get; set; }
+
+        [DataMember]
+        public string CreatedBy { get; set; }
+
+        [DataMember]
+        public DateTime CreatedOn { get; set; }
+
+        [DataMember]
+        public string ModifiedBy { get; set; }
+
+        [DataMember]
+        public DateTime ModifiedOn { get; set; }
+    }
+}
