fc5b4906c7f3560f9c62924fbbb32323d51ad9ed
bkpabi <bkpabi@gmail.com>
2014-07-04 16:22:39 +0530

    Added user manager class

---
+++ Compass.Business.Components/UserManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compass.Business.Components
+{
+    public class UserManager
+    {
+        public int CreateNewUser()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
